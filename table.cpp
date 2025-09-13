dba6f68a856a6a361d1abd9398cd4b6de67ac2f4
sagar-177 <sagar17789907@gmail.com>
2025-09-13 14:10:22 +0530

    Program for Table

---
+++ table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int i,n;      //initialization of vairables.
+cout << "Enter any Number = ";
+cin >> n;
+
+for(i = 1; i<=10; i++)
+{
+    cout << n*i << endl;
+}
+
+    return 0;
+}
