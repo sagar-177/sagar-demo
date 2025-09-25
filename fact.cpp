b5c58b8d9abf2401d346e2a5aecaf1ccb13a56fe
sagar-177 <sagar17789907@gmail.com>
2025-09-25 21:05:35 +0530

    Factorial Program

---
+++ fact.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+int num,fact=1;
+
+cout << "Enter any Number = ";
+cin >> num;
+
+for(int i = num; i >= 1; i--)
+{
+    fact*=i;
+}
+
+cout << "Factorial of " << num << " = " << fact;
+
+    return 0;
+}
